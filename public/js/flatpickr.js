694c0f2dd9e8bb01c4f9f3c3ab5e63c4a34fa058
normal-yoko <gujiguji399@gmail.com>
2025-04-18 11:06:01 +0900

    laravel-tabelog-kadai

--- public/js/flatpickr.js
+++ public/js/flatpickr.js
@@ -2,7 +2,7 @@ flatpickr('#reservation_date', {
   locale: 'ja',
   minDate: 'today',
   maxDate: new Date().fp_incr(60),
-  // その店舗の定休日を選択不可にする
+  // その店舗の定休日の選択を不可にする
   disable: [
     function (date) {
       return restaurantRegularHolidays.includes(date.getDay());
